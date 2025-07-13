e6cf37cb3ff0ed0318504eb3eb6eeede066e6e8a
Matheka25427 <petermatheka251@gmail.com>
2025-07-13 20:15:23 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,14 @@
 $ node 4-concat.js c cool
 c is cool
-
+console.log()
 $ node 4-concat.js c
 c is undefined
 
 $ node 4-concat.js
 undefined is undefined
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + ' is ' + arg2);
