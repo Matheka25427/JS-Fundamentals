4340a2ba2f3b7fac0473f5fcd7433e87efaaa1f5
Matheka25427 <petermatheka251@gmail.com>
2025-07-13 20:48:53 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,26 +1,8 @@
-$ node 4-concat.js c cool
-c is cool
-console.log()
-$ node 4-concat.js c
-c is undefined
-
-$ node 4-concat.js
-undefined is undefined
 #!/usr/bin/node
 
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 console.log(arg1 + ' is ' + arg2);
-#!/usr/bin/node
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
 
-if (arg1 === undefined) {
-  console.log("File is present");
-} else if (arg2 === undefined) {
-  console.log("HBTN");
-} else {
-  console.log("Python fun");
-}
